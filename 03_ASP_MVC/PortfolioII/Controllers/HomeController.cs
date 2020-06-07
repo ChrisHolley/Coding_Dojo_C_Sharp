d4e6eda93959a6cc6b3120d583c56f56dc763ebc
ChrisHolley <asiholley@gmail.com>
2020-06-07 00:06:43 -0700

    complete portfolioII assignment. Learn how to use static files, controllers, viewresults

---
+++ 03_ASP_MVC/PortfolioII/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PortfolioII
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public ViewResult Home()
+        {
+            //server will look for WellHelloThere.cshtml if the method name is WellHelloThere()
+            //server will look in Views folder
+            //then folder of controller name: Home
+            //then the .cshtml file matching the method name: WellHelloThere() = WellHelloThere.cshtml
+            //if the file can't be found in the above path, ASP will search in the other View folders
+            return View();
+            //you can also put in View("WellHelloThere")
+            //ASP will look for WellHelloThere.cshtml in all the View directories
+        }
+        [HttpGet("contact")]
+        public ViewResult Contact()
+        {
+            return View();
+        }
+    }
+}
